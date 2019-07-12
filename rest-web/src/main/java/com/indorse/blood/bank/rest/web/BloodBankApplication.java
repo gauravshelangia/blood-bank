407a511fe51ffb9863e2755080702f7e65fee2a9
Gaurav Yadav <gaurav.yadav@webklipper.com>
2019-07-13 01:53:08 +0530

    First commit setting up empty project

---
+++ rest-web/src/main/java/com/indorse/blood/bank/rest/web/BloodBankApplication.java
@@ -0,0 +1,13 @@
+package com.indorse.blood.bank.rest.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankApplication.class, args);
+	}
+
+}
